1fb56c06797501bb6c232e27ca2195d1c74e2a59
= <=>
2022-10-09 20:54:52 -0300

    Utilizando componente de forma local e global

---
+++ componente-intro/src/Contadores.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="contadores">
+    <h1>Contadores</h1>
+    <app-contador v-for="c in 5" :key="c" />
+  </div>
+</template>
+
+<script>
+//importando o componente
+import Contador from './Contador.vue'
+
+export default {
+    //registrando o componente Contador de forma local
+    components: {
+        'app-contador': Contador
+    }
+};
+
+//precisa ter a key(c) na hora de fazer o loop do componente
+</script>
+
+<style>
+</style>
--- componente-intro/src/main.js
+++ componente-intro/src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 //registrando o componente Contador, vai receber o que está em exports
-import Contador from './Contador.vue'
+import Contadores from './Contadores.vue'
 
 Vue.config.productionTip = false
 //utilizando o componente
 //importando de forma global
 //nome-do-componente(tag), o Componente que foi importado
-Vue.component('app-contador', Contador)
+Vue.component('app-contadores', Contadores)
 
 new Vue({
   render: h => h(App),
