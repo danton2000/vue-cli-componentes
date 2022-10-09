602447e4063e89081a30194a180d5d8f2cfe7a51
= <=>
2022-10-09 20:43:06 -0300

    Criando o componente contador

--- componente-intro/src/main.js
+++ componente-intro/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+//registrando o componente Contador, vai receber o que está em exports
+import Contador from './Contador.vue'
 
 Vue.config.productionTip = false
+//utilizando o componente
+//importando de forma global
+//nome-do-componente(tag), o Componente que foi importado
+Vue.component('app-contador', Contador)
 
 new Vue({
   render: h => h(App),
